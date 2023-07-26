13b7f86b0cd0bd5835c276212e64658543f6c0be
Brandon Jensen <brandon.l.jensen@gmail.com>
2023-07-26 06:25:45 +0000

    adds ops challenge 02

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script
+# Author:                       Brandon Jensen
+# Date of latest revision:      07/25/2023
+# Purpose:                      Create the script that will print “hello world” to the screen.
+
+# Declaration of variables
+
+# Main
+echo "hello world"
+
+# End
